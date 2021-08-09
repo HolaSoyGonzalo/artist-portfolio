a7883c4fec1cf425712611222994e956485206ca
Davide Pacco <72199035+HolaSoyGonzalo@users.noreply.github.com>
2021-08-09 12:37:27 +0300

    Contact Form Up and Working

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+
+import ContactForm from "../Components/ContactForm";
+
 function Contact() {
-  return <Container>NUMBER 3</Container>;
+  return (
+    <Container>
+      <ContactForm />
+    </Container>
+  );
 }
 
 export default Contact;
 
 const Container = styled.div`
-  height: 100vh;
-  background-color: blue;
+  height: 87vh;
+  padding-top: 140px;
+  margin: 0 auto;
+  text-align: center;
 `;
